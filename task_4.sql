7fbbfb6496c02243959c4a23964bfc1dccf6ec20
Ermi539 <ermiashabtamu477@gmail.com>
2024-07-28 09:15:54 -0700

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -6,7 +6,7 @@ SELECT
     COLUMN_KEY AS 'Key',
     EXTRA AS 'Extra Information'
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
     AND TABLE_NAME = 'Books'
